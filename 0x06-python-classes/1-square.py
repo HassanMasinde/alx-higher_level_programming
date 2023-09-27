63edd27d9cbd61e8f16b40ad6448e652f0b65315
HassanMasinde <wesongahassan15@gmail.com>
2023-09-27 05:00:05 -0700

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+# 0-square.py by UDO INNOCENT
+"""Defines a square """
+
 
 class Square:
-    """Defines a square"""
+    """represents  Square"""
 
-    def __init__(self, size=0):
-        """Initializes a square with a specific size."""
+    def __init__(self, size):
+        """initializing this Square class
+        Args: size - represnets the size of the square defined
+        """
+
         self.__size = size
